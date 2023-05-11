3a0c25bbd2db3e45c96de7a2a3ce6e91c83cf107
susmithasreemathi <susmitha.k@irohub.com>
2023-05-11 15:22:00 +0530

    bottom navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -19,13 +19,11 @@ class _MyAppState extends State<MyApp> {
         theme: ThemeData(primarySwatch: Colors.green),
         debugShowCheckedModeBanner: false,
         // home : new ListViewBuilder(), NO Need To Use Unnecessary New Keyword
-        home:  ListViewBuilder());
+        home: ListViewBuilder());
   }
 }
 
 class ListViewBuilder extends StatefulWidget {
-
- 
   @override
   State<ListViewBuilder> createState() => _ListViewBuilderState();
 }
@@ -33,6 +31,76 @@ class ListViewBuilder extends StatefulWidget {
 class _ListViewBuilderState extends State<ListViewBuilder> {
   int _currentIndex = 0;
 
+  static const List<Widget> _pages = <Widget>[
+    Icon(
+      Icons.call,
+      size: 150,
+    ),
+    Icon(
+      Icons.camera,
+      size: 150,
+    ),
+    Icon(
+      Icons.chat,
+      size: 150,
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("ListView.builder")),
+      
+      body: SafeArea(
+          child: Center(
+        child: _pages.elementAt(_currentIndex), //New
+      )),
+     
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.grey,
+        onTap: (newIndex) {
+          setState(() {
+            _currentIndex = newIndex;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+              backgroundColor: Colors.blue),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: 'Account',
+              backgroundColor: Colors.pink),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              label: 'Search',
+              backgroundColor: Colors.purple),
+        ],
+      ),
+    );
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+
+    _currentIndex = index;
+   
+      
+    });
+  }
+}
+
+class listnn extends StatefulWidget {
+  const listnn({super.key});
+
+  @override
+  State<listnn> createState() => _listnnState();
+}
+
+class _listnnState extends State<listnn> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -48,29 +116,12 @@ class _ListViewBuilderState extends State<ListViewBuilder> {
                 onTap: () {
                   Navigator.push(
                     context,
-                    MaterialPageRoute(builder: (context) => const MyApp2 ()),
+                    MaterialPageRoute(builder: (context) => const MyApp2()),
                   );
                 },
               );
             }),
           ),
-        )
-        ),
-             bottomNavigationBar: BottomNavigationBar(
-          currentIndex: _currentIndex,
-          selectedItemColor: Colors.blue,
-          unselectedItemColor: Colors.grey,
-          onTap: (newIndex) {
-            setState(() {
-              _currentIndex = newIndex;
-            });
-          },
-          items: const [
-            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-            BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Account'),
-            BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
-          ],
-        ),
-        );
+        )));
   }
 }
